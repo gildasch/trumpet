ccd1b7a92ac53aa7eaf4dd5884d1ab52962fa4db
Gildas Chabot <gildas.chabot@schibsted.com>
2019-02-08 16:59:14 +0100

    move the trumpet functions into Trumpet class

---
+++ trumpet.js
@@ -0,0 +1,45 @@
+function Trumpet() {
+  this.valves = ["j", "k", "l"]
+  this.modifiers = ["v", "d", "f", "g", "e", "r", "t", "y"]
+
+  this.note = function(keys) {
+    valve1 = keys.get(this.valves[0]) == true
+    valve2 = keys.get(this.valves[1]) == true
+    valve3 = keys.get(this.valves[2]) == true
+
+    var modifier = -1
+
+    for (var i = this.modifiers.length-1; i >= 0; i--) {
+      if (keys.get(this.modifiers[i])) {
+        modifier = i+1
+        break
+      }
+    }
+
+    if (modifier == -1) {
+      return 0
+    }
+
+    return this.calculateNote(modifier, valve1, valve2, valve3)
+  }
+
+  this.calculateNote = function(modifier, valve1, valve2, valve3) {
+    var halftones = 0
+
+    if (valve1) {
+      halftones -= 2
+    }
+    if (valve2) {
+      halftones -= 1
+    }
+    if (valve3) {
+      halftones -= 3
+    }
+
+    return this.frequency(modifier, halftones)
+  }
+
+  this.frequency = function(modifier, halftones) {
+    return 261.626 * (modifier/2) * Math.pow(2, halftones / 12)
+  }
+}
